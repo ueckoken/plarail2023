40f66b9b40ba0362ee0bd01c59bbcacb3d757b92
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-10-24 21:06:44 +0900

    implement stop and point

--- backend/state-manager/pkg/db/db_test.go
+++ backend/state-manager/pkg/db/db_test.go
@@ -1,39 +1,92 @@
 package main
 
 import (
-	"testing"
-
 	"github.com/joho/godotenv"
+	statev1 "github.com/ueckoken/plarail2023/backend/spec/state/v1"
+	"testing"
 )
 
-func TestDBAdd(t *testing.T) {
-	// mongodb add test using db.go
-	godotenv.Load(".env")
+func Test_ConnectDB(t *testing.T) {
+	defer c()
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
 	open()
-	defer close()
-	book := Book{
-		Title:  "Test Book",
-		Author: "Test Author",
-	}
-	err := addBook(book)
+}
+
+func Test_SetPoint(t *testing.T) {
+	defer c()
+	err := godotenv.Load(".env")
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
+	open()
+	setPoint(&statev1.PointAndState{
+		Id:    "test",
+		State: statev1.PointStateEnum_POINT_STATE_REVERSE,
+	})
+	point := getPoint("test")
+	if point.State != statev1.PointStateEnum_POINT_STATE_REVERSE {
+		t.Fatal("point state is not reverse")
+	}
+}
 
-	result, err := getBook(book.Title)
+func Test_SetStop(t *testing.T) {
+	defer c()
+	err := godotenv.Load(".env")
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
-
-	// check if book is a match
-	if result.Author != book.Author && result.Title != book.Title {
-		t.Error("Book title does not match")
+	open()
+	setStop(&statev1.StopAndState{
+		Id:    "test",
+		State: statev1.StopStateEnum_STOP_STATE_GO,
+	})
+	stop := getStop("test")
+	if stop.State != statev1.StopStateEnum_STOP_STATE_GO {
+		t.Fatal("point state is not stop")
 	}
+}
 
-	defer func() {
-		err = deleteBook(book)
-		if err != nil {
-			t.Error(err)
-		}
-	}()
+func Test_GetPoints(t *testing.T) {
+	defer c()
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	open()
+	setPoint(&statev1.PointAndState{
+		Id:    "test",
+		State: statev1.PointStateEnum_POINT_STATE_REVERSE,
+	})
+	setPoint(&statev1.PointAndState{
+		Id:    "test2",
+		State: statev1.PointStateEnum_POINT_STATE_NORMAL,
+	})
+	points := getPoints()
+	if len(points) < 2 {
+		t.Fatal("points length is not larger than 2")
+	}
+}
+
+func Test_GetStops(t *testing.T) {
+	defer c()
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	open()
+	setStop(&statev1.StopAndState{
+		Id:    "test",
+		State: statev1.StopStateEnum_STOP_STATE_GO,
+	})
+	setStop(&statev1.StopAndState{
+		Id:    "test2",
+		State: statev1.StopStateEnum_STOP_STATE_STOP,
+	})
+	stops := getStops()
+	if len(stops) < 2 {
+		t.Fatal("stops length is not larger than 2")
+	}
 }
