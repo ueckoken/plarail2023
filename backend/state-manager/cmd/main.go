f49489e127ef77738ab0344458d5402f399ad9ea
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-10-26 16:56:14 +0900

    update handler

--- backend/state-manager/cmd/main.go
+++ backend/state-manager/cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/joho/godotenv"
+	mqtt "github.com/ueckoken/plarail2023/backend/state-manager/pkg/mqtt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	mqtt.StartHandler()
 }
--- backend/state-manager/pkg/mqtt/mqtt.go
+++ backend/state-manager/pkg/mqtt/mqtt.go
@@ -1,8 +1,10 @@
 package mqtt
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -33,4 +35,43 @@ func Subscribe(cc mqtt.Client, topic string, f mqtt.MessageHandler) {
 func Send(cc mqtt.Client, topic string, payload string) {
 	token := cc.Publish(topic, 0, false, payload)
 	token.Wait()
+}
+
+func StartHandler() {
+	msgCh := make(chan mqtt.Message)
+	var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		msgCh <- msg
+	}
+	cc := MakeClient()
+	Subscribe(cc, "point/#", f)
+	Subscribe(cc, "stop/#", f)
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+
+	for {
+		select {
+		case msg := <-msgCh:
+			// if topic start with "point/"
+			if msg.Topic()[0:6] == "point/" {
+				pointHandler(msg)
+			}
+			// if topic start with "stop/"
+			if msg.Topic()[0:5] == "stop/" {
+				stopHandler(msg)
+			}
+		case <-signalCh:
+			fmt.Println("Interrupted")
+			cc.Disconnect(1000)
+			return
+		}
+	}
+}
+
+func pointHandler(msg mqtt.Message) {
+	log.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+func stopHandler(msg mqtt.Message) {
+	log.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 }
