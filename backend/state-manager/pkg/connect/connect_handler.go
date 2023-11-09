fb667c6405ca6803b2f2e755aec71c19e0a8ea26
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2023-11-09 19:59:45 +0900

    contextを受ける

--- backend/state-manager/pkg/connect/connect_handler.go
+++ backend/state-manager/pkg/connect/connect_handler.go
@@ -3,8 +3,11 @@ package connect_handler
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"connectrpc.com/connect"
 	statev1 "github.com/ueckoken/plarail2023/backend/spec/state/v1"
@@ -167,18 +170,30 @@ func (s *StateManagerServer) UpdateTrainUUID(
 	return nil, err
 }
 
-func StartHandler() {
+func StartHandler(ctx context.Context) {
 	server := &StateManagerServer{}
 	mux := http.NewServeMux()
 	path, handler := statev1connect.NewStateManagerServiceHandler(server)
 	mux.Handle(path, handler)
-	err := http.ListenAndServe(
-		"0.0.0.0:8080",
-		// Use h2c so we can serve HTTP/2 without TLS.
-		h2c.NewHandler(mux, &http2.Server{}),
-	)
-	if err != nil {
-		log.Fatalln(err)
-		return
+	srv := &http.Server{
+		Addr:    net.JoinHostPort("0.0.0.0", "8080"),
+		Handler: h2c.NewHandler(mux, &http2.Server{}),
+	}
+	errC := make(chan error)
+	go func() {
+		err := srv.ListenAndServe()
+		errC <- err
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			newCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer cancel()
+			srv.Shutdown(newCtx)
+			return
+		case err := <-errC:
+			log.Println(fmt.Errorf("failed to start http server: %w", err))
+			return
+		}
 	}
 }
