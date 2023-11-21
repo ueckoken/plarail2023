da7741785ea255c3e4679b4674f17ef93207eb7a
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-11-21 18:17:47 +0900

    Update buf.gen.yaml and page.tsx files

--- frontend/dashboard/app/test/page.tsx
+++ frontend/dashboard/app/test/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import {createPromiseClient} from "@connectrpc/connect";
+import {StateManagerService} from "@/proto/state/v1/state_connectweb";
+import {createConnectTransport} from "@bufbuild/connect-web";
+
 export default function Test() {
+
+    const transport = createConnectTransport(
+        {
+            // baseUrl: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+            baseUrl: "http://localhost:8080",
+        });
+    const sendData = () => {
+        (async () => {
+            const client = createPromiseClient(StateManagerService, transport);
+            const res = await client.getBlockStates({});
+            console.log(res);
+        })();
+    }
+
     return (
         <div>
-            
+            <h1>Test</h1>
+            <button onClick={sendData}>Test</button>
         </div>
     )
 }
