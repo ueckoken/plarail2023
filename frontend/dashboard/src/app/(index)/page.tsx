ca5b97c52c111edf4927884a5bf19d00765a55a4
Mimi <mimi@monika.localdomain>
2023-11-23 07:57:02 +0900

    trainコンポーネントを修正

--- frontend/dashboard/src/app/(index)/page.tsx
+++ frontend/dashboard/src/app/(index)/page.tsx
@@ -12,8 +12,32 @@ export default function Home() {
         <>
             <svg width="100%" height="620px">
                 <Background />
-                <Platform position={{ x: 150, y: 50 }} name="駅" />
-                <Train position={{ x: 50, y: 50 }} trainData={{ id: "id" }} />
+                <Rail
+                    points={[
+                        { x: 50, y: 100 },
+                        { x: 250, y: 100 },
+                        { x: 250, y: 200 },
+                        { x: 50, y: 200 },
+                        { x: 50, y: 100 },
+                    ]}
+                />
+                <SwitchPoint
+                    position={{ x: 250, y: 150 }}
+                    fromAngle={0}
+                    leftOutAngle={90}
+                    rightOutAngle={-90}
+                    isLeft={true}
+                />
+                <Platform position={{ x: 150, y: 150 }} name="新線新宿" />
+                <TraficLight position={{ x: 150, y: 200 }} isStop={false} />
+                <Train
+                    position={{ x: 150, y: 100 }}
+                    trainData={{ id: "keio", type: 2 }}
+                />
+                <Train
+                    position={{ x: 150, y: 200 }}
+                    trainData={{ id: "shinjyuku", type: 1 }}
+                />
             </svg>
         </>
     );
