f371aea1f9f7328e990a99ec2324f7703aeccf09
gotti <gotti@mail.gotti.dev>
2023-11-24 18:23:19 +0900

    use vite

--- frontend/dashboard/src/components/mapview/mapview.tsx
+++ frontend/dashboard/src/components/mapview/mapview.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
     Rail,
     Platform,
@@ -8,10 +6,12 @@ import {
     Background,
     Train,
 } from "@/components/svgElements";
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { getPointStates, getStopStates, getTrains, updatePointState, updateStopState } from '@/proto/state/v1/state-StateManagerService_connectquery'
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
+import { addTrain, getBlockStates, getPointStates, getStopStates, getTrains, updatePointState, updateStopState } from '@/proto/state/v1/state-StateManagerService_connectquery'
 import { StopStateEnum } from "@/proto/state/v1/stop_pb";
-import {PointStateEnum} from "@/proto/state/v1/point_pb";
+import { PointStateEnum } from "@/proto/state/v1/point_pb";
+import { BlockState, BlockStateEnum } from "@/proto/state/v1/block_pb";
+import {Train as TrainPB} from "@/proto/state/v1/train_pb";
 
 const STOP_RAILS_FROM_ID: Map<string, {x: number, y: number}> = new Map([
   ["shinjyuku_up_s1", { x: 320, y: 400 }],
@@ -36,16 +36,58 @@ const POINT_RAILS_FROM_ID: Map<string, {x: number, y: number, fixed: number, str
   ["chofu_up_p1", { x: 1000, y: 140, fixed: -90, straight: 90, changed: 135 }], // Question: これdownじゃない？
 ]);
 
-const LOCATIONS_FOR_TRAIN_FROM_ID: Map<string, {x: number, y: number}> = new Map([...STOP_RAILS_FROM_ID, ...POINT_RAILS_FROM_ID])
+const BLOCKS_FROM_ID: Map<string, {x: number, y: number}> = new Map([
+  ["shinjyuku_down_b1", { x: 320, y: 400 }],
+  ["sakurajosui_down_b1", {x: 320, y: 500}],
+  ["chofu_down_b2", {x: 200, y: 300}],
+  ["hashimoto_down_b1", {x: 200, y: 140}],
+  ["hashimoto_up_b1", {x: 200, y: 220}],
+  ["chofu_up_b1", {x: 840, y: 220}],
+  ["chofu_down_b1", {x: 1000, y: 220}],
+  ["hachioji_down_b1", {x: 700, y: 330}],
+  ["hachioji_up_b1", {x: 700, y: 410}],
+  ["sakurajosui_up_b1", {x: 700, y: 560}],
+  ["shinjyuku_up_b1", {x: 700, y: 560}],
+])
 
-export default function Home() {
-    const { data: stops, refetch: refetchStops } = useQuery(getStopStates.useQuery({}));
+const LOCATIONS_FOR_TRAIN_FROM_ID: Map<string, {x: number, y: number}> = new Map([...STOP_RAILS_FROM_ID, ...BLOCKS_FROM_ID]);
+
+function MapView() {
+    const { data: stops, refetch: refetchStops } = useSuspenseQuery(getStopStates.useQuery({}));
     const { mutate: mutateStops } = useMutation(updateStopState.useMutation({}));
-    const { data: points, refetch: refetchPoints } = useQuery(getPointStates.useQuery({}));
+    const { data: points, refetch: refetchPoints } = useSuspenseQuery(getPointStates.useQuery({}));
     const { mutate: mutatePoints } = useMutation(updatePointState.useMutation({}));
-    const { data: trains } = useQuery(getTrains.useQuery({}));
+    const { data: trains } = useSuspenseQuery(getTrains.useQuery({}));
+    const { mutate: mutateTrain } = useMutation(addTrain.useMutation({}));
+    const { data: blocks } = useSuspenseQuery(getBlockStates.useQuery({}));
+
+    // blocks to Map
+    const blocksMap = new Map<string, BlockState>();
+    blocks?.states.forEach((block) => {
+      blocksMap.set(block.blockId, block);
+    });
+
+    function isClosed(blockId: string): boolean {
+      const s = blocksMap.get(blockId)?.state;
+      if (s == undefined) {
+        console.log(`blockId ${blockId} is not found`);
+      }
+      return s === BlockStateEnum.BLOCK_STATE_CLOSE;
+    }
+    const nextLocation = (train: TrainPB): TrainPB | undefined => {
+      const current = train.positionId;
+      LOCATIONS_FOR_TRAIN_FROM_ID.forEach((_, key) => {
+        if (key === current) {
+          const next = LOCATIONS_FOR_TRAIN_FROM_ID.keys().next().value;
+          return { ...train, positionId: next };
+        }
+      });
+      return undefined
+    }
     return (
         <>
+            <button onClick={() => mutateTrain({ train: { trainId: "test", positionId: "shinjyuku_up_s1" }})}>Add Train</button>
+            <button onClick={() => mutateTrain({ train: { trainId: "test", positionId: nextLocation(trains.trains[0])?.positionId} })}>Move</button>
             <svg width="100%" height="620px">
                 <Background />
                 <Rail
@@ -55,23 +97,79 @@ export default function Home() {
                       {x: 320, y: 500},
                       {x: 200, y: 500},
                       {x: 200, y: 400},
+                    ]}
+                    isClosed={isClosed("shinjyuku_down_b1")}
+                />
+                <Rail
+                    points={[
+                      {x: 200, y: 300},
+                      {x: 200, y: 400},
+                    ]}
+                    isClosed={isClosed("shinjyuku_up_b1")}
+                />
+                <Rail
+                    points={[
+                      {x: 200, y: 300},
+                      {x: 200, y: 140},
+                    ]}
+                />
+                <Rail
+                    points={[
+                      {x: 200, y: 140},
+                      {x: 200, y: 30},
                       {x: 200, y: 30},
                       {x: 1000, y: 30},
-                      {x: 1000, y: 180},
+                      {x: 1000, y: 140},
+                    ]}
+                    isClosed={isClosed("sakurajosui_down_b1")}
+                />
+                <Rail
+                    points={[
+                      {x: 1000, y: 140},
+                      {x: 1000, y: 220},
+                    ]}
+                />
+                <Rail
+                    points={[
+                      {x: 1000, y: 220},
                       {x: 1000, y: 560},
+                      {x: 700, y: 560},
+                    ]}
+                    isClosed={isClosed("chofu_down_b1")}
+                />
+                <Rail
+                    points={[
                       {x: 700, y: 560},
                       {x: 600, y: 560},
                       {x: 600, y: 480},
                       {x: 700, y: 480},
+                    ]}
+                    isClosed={isClosed("hachioji_down_b1")}
+                />
+                <Rail
+                    points={[
+                      {x: 700, y: 480},
                       {x: 840, y: 480},
-                      {x: 840, y: 180},
+                      {x: 840, y: 220},
+                    ]}
+                    isClosed={isClosed("hachioji_up_b1")}
+                />
+                <Rail
+                    points={[
+                      {x: 840, y: 220},
                       {x: 840, y: 80},
                       {x: 320, y: 80},
-                      {x: 320, y: 180},
-                      {x: 320, y: 400},
+                      {x: 320, y: 140},
                     ]}
+                    isClosed={isClosed("chofu_up_b1")}
                 />
                 <Platform isHorizontal={false} position={{ x: 260, y: 220 }} name="桜上水" />
+                <Rail
+                    points={[
+                      {x: 320, y: 140},
+                      {x: 320, y: 300},
+                    ]}
+                />
                 <Rail
                     points={[
                       {x: 320, y: 140},
@@ -79,7 +177,6 @@ export default function Home() {
                       {x: 360, y: 220},
                       {x: 360, y: 270},
                       {x: 320, y: 300},
-                      {x: 320, y: 400},
                     ]}
                 />
                 <Rail
@@ -90,6 +187,13 @@ export default function Home() {
                       {x: 160, y: 270},
                       {x: 200, y: 300},
                       ]}
+                />
+                <Rail
+                    points={[
+                      {x: 320, y: 300},
+                      {x: 320, y: 400},
+                    ]}
+                    isClosed={isClosed("sakurajosui_up_b1")}
                 />
                 <Platform isHorizontal={false} position={{ x: 900, y: 220 }} name="調布" />
                 <Rail
@@ -150,3 +254,5 @@ export default function Home() {
         </>
     );
 }
+
+export default MapView
