7c301d8ac650d6f96061c77d5255d5a767076434
Koichi Hirachi <kouichi.hirachi@gmail.com>
2024-04-28 19:56:39 +0900

    fix bugs

--- frontend/dashboard/src/App.tsx
+++ frontend/dashboard/src/App.tsx
@@ -12,19 +12,11 @@ class ErrorBoundary extends React.Component {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static getDerivedStateFromError(error: any) {    // Update state so the next render will show the fallback UI.    
-    console.log(error);
-    return { hasError: true };
-  }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  componentDidCatch(error: any, errorInfo: any) {    // You can also log the error to an error reporting service   
-    console.log(error, errorInfo);
-  }
   render() {
     if (this.state.hasError) {      // You can render any custom fallback UI      
       return <h1>Something went wrong.</h1>;
     }
-    return this.props.children;
+    return (this.props as { children: React.ReactNode }).children;
   }
 }
 
